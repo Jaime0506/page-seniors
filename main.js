67553e97f4e7b27eb21ed6feb9ef3848121f6614
Jaime Mejia <jaimemejia1003@gmail.com>
2022-01-12 12:47:56 -0500

    12/01/2022

--- main.js
+++ main.js
@@ -5,7 +5,24 @@ const pushbar = new Pushbar({
     overlay: true,
 })
 
-//DIMENSSIONS PAGE SECTIONS
+// FUNCTION DOWNLOAD PDF-------------------------------------------------------------------------
+
+function downloadPDF (path, name){
+
+    const downloadInstance = document.createElement("a");
+    
+    downloadInstance.href = path;
+    downloadInstance.target = "_blanck";
+    downloadInstance.download = name;
+
+    document.body.appendChild(downloadInstance);
+    downloadInstance.click();
+    document.body.removeChild(downloadInstance);
+
+};
+
+
+//DIMENSSIONS PAGE SECTIONS------------------------------------------------------------------
 
 const heightWindows = window.innerHeight - 60;
 
@@ -21,49 +38,19 @@ document.getElementById("threePage").style.height = heightWindows + "px";
 document.getElementById("content-fourPage").style.height = heightWindows  + "px";
 document.getElementById("fourPage").style.height = heightWindows  + "px";
 
-//PUSHBAR OPEN
+//PUSHBAR-------------------------------------------------------------------------------
 
+//OPEN
 document.querySelector(".btn-menu").addEventListener("click", () => {
     pushbar.open("menu");
 });
 
-//PUSHBAR CLOSE
-
+//CLOSE
 document.querySelector(".imageExit").addEventListener("click", () => {
     pushbar.close();
 });
 
-//CARDS
-
-const cardNurse = document.querySelector(".nurse");
-const cardBoss = document.querySelector(".boss");
-const cardDoctor = document.querySelector(".doctor");
-
-//MODALS OPTIONS
-
-const buttonCloseModal = document.getElementById("closeModal");
-const modal = document.getElementById("containerModal"); 
-
-const modalTitle = document.getElementById("modalTitle");
-const modalSubtitle = document.getElementById("modalSubtitle");
-
-//CLOSE MODAL
-
-buttonCloseModal.addEventListener("click", function () {
-    modal.classList.remove("showModal");
-    modal.style.pointerEvents = "none";
-});
-
-//WHATSAPP API
-
-const buttonWhatsapp = document.querySelector(".buttonWhatsapp");
-
-buttonWhatsapp.addEventListener("click", () => {
-    window.open("https://api.whatsapp.com/send?phone=+571234567890&text=Hola!%20Quisiera%20Obtener%20Mas%20Informacion");
-});
-
-//PUSHBAR FUNCTIONS SCROLL
-
+//FUNCTIONS SCROLL
 document.querySelector(".rInicio").addEventListener("click", function() {
     window.scroll({
         top: 0,
@@ -100,43 +87,122 @@ document.querySelector(".rUbicacion").addEventListener("click", () => {
     pushbar.close();
 });
 
-//OPEN MODALS INFO
+//MODALS - CARDS-------------------------------------------------------------------------
 
-const contentModals = {
+const cardNurse = document.querySelector(".nurse");
+const cardBoss = document.querySelector(".boss");
+const cardDoctor = document.querySelector(".doctor");
+
+const buttonCloseModal = document.getElementById("closeModal");
+const modal = document.getElementById("containerModal"); 
+
+const modalOffice = document.getElementById("modalOffice");
+const modalTitle = document.getElementById("modalTitle");
+const modalSubtitle = document.getElementById("modalSubtitle");
+
+const modalImage = document.getElementById("modalImage");
+
+const buttonDownloadPDF = document.getElementById("buttonDownloadPDF");
+
+const dataModals = {
 
     nurse: {
         office: "Enfermera Jefe",
         title: "Maria Del Perpetuo Socorro",
         subtitle: "Maria es una profesional de la salud, con mas de 5 años de experiencia, que cuenta con las capacidades necesarias para cuidar a nuestros viejitos, con la mayor disposicion posible.",
+        image: "/img/nurseImageExample.jpg",
+        path: "./sample.pdf",
+        name: "Cv Maria Socorro",
     },
 
     ceo: {
         office: "Lider del Proyecto",
         title: "Olga Lucia Ramirez",
         subtitle: "Olga lucia, jefe de nuestro proyecto, se encarga de gestionar y orientar a todo el equipo en armonia, para poder funcioanr y ejercer de la mejor manera posible.",
+        image: "/img/ceoExample.jpg",
+        path: "./sample.pdf",
+        name: "Cv Olga Lucia",
     },
 
     doctor: {
         office: "Doctor",
         title: "Jose Rodolfo Mayorquin",
         subtitle: "Jose Rodolfo, un medico profesional, que esta con toda la disposicion posible, para atender y prestar el devido servicio de salud a nuestros viejitios, con la mejor calidad posible.",
+        image: "/img/bossDoctor.jpg",
+        path: "./sample.pdf",
+        name: "Cv Jose Rodolfo",
     }
 
 };
 
 cardNurse.addEventListener("click", () => {
+    
+    modalOffice.innerText = dataModals.nurse.office;
+    modalTitle.innerText = dataModals.nurse.title;
+    modalSubtitle.innerText = dataModals.nurse.subtitle;
+
+    //IMAGE
+    modalImage.setAttribute("src", dataModals.nurse.image);
+
+    //DOWNLOAD PDF
+    buttonDownloadPDF.addEventListener("click", function() {
+        downloadPDF(dataModals.nurse.path, dataModals.nurse.name);
+    })
+    
     modal.classList.add("showModal");
     modal.style.pointerEvents = "all";
+
 });
 
 cardBoss.addEventListener("click", () => {
+
+    modalOffice.innerText = dataModals.ceo.office;
+    modalTitle.innerText = dataModals.ceo.title;
+    modalSubtitle.innerText = dataModals.ceo.subtitle;
+
+    //IMAGE
+    modalImage.setAttribute("src", dataModals.ceo.image);
+    
+    //DOWNLOAD PDF
+    buttonDownloadPDF.addEventListener("click", function() {
+        downloadPDF(dataModals.ceo.path, dataModals.ceo.name);
+    })
+
     modal.classList.add("showModal");
     modal.style.pointerEvents = "all";
+
 });
 
 cardDoctor.addEventListener("click", () => {
+
+    modalOffice.innerText = dataModals.doctor.office;
+    modalTitle.innerText = dataModals.doctor.title;
+    modalSubtitle.innerText = dataModals.doctor.subtitle;
+
+    //IMAGE
+    modalImage.setAttribute("src", dataModals.doctor.image);
+    
+    //DOWNLOAD PDF
+    buttonDownloadPDF.addEventListener("click", function() {
+        downloadPDF(dataModals.doctor.path, dataModals.doctor.name);
+    })
+    
     modal.classList.add("showModal");
     modal.style.pointerEvents = "all";
+
 });
 
+//CLOSE MODAL------------------------------------------------------------------------------
 
+buttonCloseModal.addEventListener("click", function () {
+    modal.classList.remove("showModal");
+    modal.style.pointerEvents = "none";
+});
+
+//WHATSAPP API-----------------------------------------------------------------------------
+
+const buttonWhatsapp = document.querySelector(".buttonWhatsapp");
+
+buttonWhatsapp.addEventListener("click", () => {
+    window.open("https://api.whatsapp.com/send?phone=+571234567890&text=Hola!%20Quisiera%20Obtener%20Mas%20Informacion");
+});
